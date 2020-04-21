4c030da178b6a2836635dc3c994dde70bf005bc9
Inari Aaltojärvi <ext.inari.aaltojarvi@cargotec.com>
2020-04-21 11:05:23 +0300

    Deployed

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ morgan.token('body', function getBody (req) {
   })
 
 app.use(express.json()) 
+app.use(express.static('build'))
 app.use(morgan(':method :url :response-time :body'))
 app.use(cors())
 
