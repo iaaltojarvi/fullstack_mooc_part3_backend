554afb373c25287d056fc21aec05749c9f63202a
Inari Aaltojärvi <ext.inari.aaltojarvi@cargotec.com>
2020-04-23 11:42:41 +0300

    New build

---
+++ build/precache-manifest.e014073863f3bf0920e37be523b9da7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9590526993b2a0c00d265a06f5b3a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "333b698748ea4f8d08b9",
+    "url": "/static/css/main.c8308061.chunk.css"
+  },
+  {
+    "revision": "198a382dee030dd8c722",
+    "url": "/static/js/2.dd9995cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dd9995cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "333b698748ea4f8d08b9",
+    "url": "/static/js/main.1c7470b9.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
